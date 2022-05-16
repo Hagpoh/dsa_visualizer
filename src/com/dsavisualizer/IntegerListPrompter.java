66962b06d65eca4d033c79640a241c128883edce
Meebly <hagpoh@gmail.com>
2022-05-16 14:25:35 -0700

    Added LinkedListVisual and the supporting algorithms add, sort, search

--- src/com/dsavisualizer/IntegerListPrompter.java
+++ src/com/dsavisualizer/IntegerListPrompter.java
@@ -3,18 +3,16 @@ package com.dsavisualizer;
 import java.util.List;
 import java.util.Scanner;
 
-public class IntegerListPrompter
-{
+public class IntegerListPrompter {
     //Fields--------------------------------------------------------------------
     //--------------------------------------------------------------------------
     private Scanner scanner = new Scanner(System.in);
     private final int valuesNeeded;
-    private List integerList;
+    private List<Integer> integerList;
 
     //Constructor---------------------------------------------------------------
     //--------------------------------------------------------------------------
-    private IntegerListPrompter(List integerList, int valuesNeeded)
-    {
+    private IntegerListPrompter(List<Integer> integerList, int valuesNeeded) {
 
         //empty collection
         //TODO: Check to ensure it is empty and not improper type (exceptions)
@@ -23,33 +21,29 @@ public class IntegerListPrompter
         this.integerList = integerList;
     }
 
-    public static IntegerListPrompter newInstance(List integerList, int valuesNeeded)
-    {
+    public static IntegerListPrompter newInstance(List<Integer> integerList, int valuesNeeded) {
         return new IntegerListPrompter(integerList, valuesNeeded);
     }
 
     //Methods-------------------------------------------------------------------
     //--------------------------------------------------------------------------
-    public void add()
-    {
-        while(true)
-        {
+    public void add() {
+        while (true) {
             System.out.print("If you wish to quit enter [Q]\n\n" +
-                                     "To add starting values to your list enter a number from 1-100:");
+                    "To add starting values to your list enter a number from 1-100:");
             String input = scanner.next();
             int number;
-            if(input.matches("\\d{1,3}"))
-            {
+            if (input.matches("\\d{1,3}")) {
                 number = Integer.parseInt(input);
-                if(number > -1 && number < 101)
+                if (number > -1 && number < 101)
                     integerList.add(number);
             }
 
             //Exit loop
             //TODO: Make sure List has sufficient enough values for algorithm
-            if(input.equals("q") && integerList.size() < valuesNeeded)
+            if (input.equals("q") && integerList.size() < valuesNeeded)
                 System.out.format("\nYou need %d values in your list.\nYou have %d. Please continue to add more values.", valuesNeeded, integerList.size());
-            else if(input.equals("q")) // AND therefore must be equal to or greater than the values needed
+            else if (input.equals("q")) // AND therefore must be equal to or greater than the values needed
                 break;
         }
     }
--- src/com/dsavisualizer/LinkedListVisual.java
+++ src/com/dsavisualizer/LinkedListVisual.java
@@ -1,9 +1,65 @@
 package com.dsavisualizer;
 
-class LinkedListVisual implements Visualizer {
+import java.util.LinkedList;
 
-    public void search() {
+public class LinkedListVisual implements Visualizer {
 
+    LinkedList<Integer> list;
+
+    public LinkedListVisual(LinkedList<Integer> list) {
+        this.list = list;
+    }
+
+    public boolean search(int searchValue) {
+        for (Integer item : list) {
+            System.out.print(item + " --> ");
+        }
+        System.out.print("\n");
+        for (Integer item : list) {
+            try {
+                if (!item.equals(searchValue)) {
+                    System.out.print(Color.RED.getColor() + item + " --> ");
+                    Thread.sleep(1000);
+                } else {
+                    System.out.print(Color.GREEN.getColor() + item + " ");
+                    System.out.println("Value found!");
+                    return true;
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Value not found!");
+        return false;
+    }
+
+    public void add(int index, int value) {
+        list.add(index, value);
+        int count = 0;
+
+        for (int i = 0; i < index; i++) {
+            String out = list.get(i) + " --> ";
+            count += out.length();
+            System.out.print(out);
+        }
+
+        for (int i = index + 1; i < list.size(); i++) {
+            String out = list.get(i) + " --> ";
+            System.out.print(out);
+        }
+
+        count += 1;
+        System.out.println();
+        String line1 = Color.GREEN.getColor() + (" ").repeat(count) + "\\ " + " /\n";
+        System.out.print(line1);
+        String line2 = (" ").repeat(count) + " \\" + "/ \n";
+        System.out.print(line2);
+        System.out.println((" ").repeat(count + 1) + value);
+
+        for (Integer integer : list) {
+            String out = Color.DEFAULT.getColor() + integer + " --> ";
+            System.out.print(out);
+        }
     }
 
     public void sort() {
