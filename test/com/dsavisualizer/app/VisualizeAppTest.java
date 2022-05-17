65d2db83fd307daedf80bf291380a3d60eee87a8
JuanCarlos Jimenez <jcj237@outlook.com>
2022-05-17 14:59:10 -0400

    Unit Testing Controller/VisualizeApp class

--- test/com/dsavisualizer/app/VisualizeAppTest.java
+++ test/com/dsavisualizer/app/VisualizeAppTest.java
@@ -2,7 +2,11 @@ package com.dsavisualizer.app;
 
 import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Scanner;
 
 public class VisualizeAppTest
@@ -17,15 +21,48 @@ public class VisualizeAppTest
         scanner = new Scanner(System.in);
     }
 
-    @Test
-    public void validInputAtDataStructurePrompt_shouldReturn()
-    {
 
+
+    @Test
+    public void validInputAfterDataStructureAndAlgorithmPrompt_shouldReturnInputRightBack()
+    {
+        String input = "2";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        assertEquals(Integer.parseInt(input), app.getIntInput());
     }
 
     @Test
-    public void validInputAtAlgorithmPrompt_shouldReturn()
+    public void invalidInputAfterDataStructureAndAlgorithmPrompt_shouldReturnInputRightBack()
     {
+        String input = "e";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
 
+        assertEquals(0  , app.getIntInput()); //0 indicates invalid input
+        //assertThrows(new NumberFormatException().getClass(), () ->  app.getIntInput());
+    }
+
+
+
+    //TODO: do more to test these methods below---------------------------------
+    //--------------------------------------------------------------------------
+    @Test
+    public void welcomeHeader_findingFile_shouldNotThrowException()
+    {
+        app.welcomeHeader();
+    }
+
+
+    //Test currently not needed but may be in the future------------------------
+    //--------------------------------------------------------------------------
+    @Test
+    public void voidMethodThatWritesToConsole_promptForAlgorithm_testNotCurrentlyNeeded()
+    {
+    }
+    @Test
+    public void voidMethodThatWritesToConsole_promptForDataStructure_testNotCurrentlyNeeded()
+    {
     }
 }
